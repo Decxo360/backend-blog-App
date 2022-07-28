421fe7d4d531b7f7795b7d0195ffa602ae343482
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-07-28 17:45:36 -0400

    rutas de mis endpoints

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const express = require('express');
 
 const app = express()
 
+
+//TODO: auth,comentaries,publications,cetegories
+//TODO: CRUD
+
+app.use('api/auth', require('./routes/auth'));
+app.use('api/categories', require('./routes/categories'));
+app.use('api/publications', require('./routes/publications'));
+app.use('api/comentaries', require('./routes/comentaries'));
+
 app.use(express.static('public'))
 
 //escuchar peticiones
-app.listen(8080,()=>{
+app.listen(process.env.PORT,()=>{
     console.log('Servidor en puerto 8080');
 })
