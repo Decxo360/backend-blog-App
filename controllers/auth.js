0f1ace74fb254abf26344f10776e029eb8188c25
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-07-29 18:01:55 -0400

    configuracion a nuestra bd

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,20 @@ const pool = require('../bd/config');
 
 
 const crearUsuario = async(req,res = express.request) =>{
-    
+    const data = req.body
+
+    try {
+
+        
+        
+    } catch (error) {
+        res.status(500).json({
+            ok:false,
+            msg: 'datos invalidos, hable con el administrador'
+        })
+    }
+
+
 }
 const borrarUsuario = async(req,res = express.request) =>{
 
