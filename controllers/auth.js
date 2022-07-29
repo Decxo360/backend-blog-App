d03cd7efcf2902170a3d12fb3ec46d7955d864f1
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-07-29 18:01:30 -0400

    metodo para la conexion a nuestra bd

---
+++ controllers/auth.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pool = require('../bd/config');
+
+
+const crearUsuario = async(req,res = express.request) =>{
+    
+}
+const borrarUsuario = async(req,res = express.request) =>{
+
+}
+const login = async(req,res = express.request) =>{
+
+}
+const register = async(req,res = express.request) =>{
+
+}
+
